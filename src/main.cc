7f2911215ece1e7314f9c249c346214c5838d96a
awrznc <49797644+awrznc@users.noreply.github.com>
2021-05-02 15:46:37 +0900

    Add sources.

---
+++ src/main.cc
@@ -0,0 +1,17 @@
+#include "cryo.hh"
+
+// The entry point.
+int main(int argc, char *argv[]) {
+
+    // Initialize CRyo.
+    const std::shared_ptr<cryo::Object> cryo(new cryo::Object(argc, argv));
+
+    // Validate Arguments.
+    if (!cryo->validate()) return 1;
+
+    // Generate Documents and Tests.
+    if (!cryo->generate()) return 2;
+
+    // SUCCESS
+    return 0;
+}
